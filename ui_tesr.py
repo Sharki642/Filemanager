7bda8ee15914399392922a2e0cb8112f91d71901
Sharki642 <bruno.landrichter@gmail.com>
2026-06-15 10:12:25 +0200

    added the new german_protokoll creation window

--- ui_tesr.py
+++ ui_tesr.py
@@ -4,6 +4,14 @@ import tkinter as tk
 print("hello world!")
 #starting the window
 root = tk.Tk()
+
+def create_protokoll_de():
+    print("Creating German Protokoll...")
+    window = tk.Toplevel(root)
+    window.title("German Protokoll")
+    window.minsize(300, 200)
+    tk.Label(window, text="Lets dreat the german protokoll").pack()
+
 #title
 root.title("FileManager")
 root.minsize(400, 400)
@@ -15,7 +23,7 @@ image = tk.PhotoImage(file="image2.png")
 tk.Label(root, image=image).pack()
 
 
-Protokoll_de = tk.Button(root, text="Create German Protokoll")
+Protokoll_de = tk.Button(root, text="Create German Protokoll", command=create_protokoll_de)
 Protokoll_de.pack()
 
 
