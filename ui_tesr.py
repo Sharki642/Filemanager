5fadde67475ed530dd32ce40301357126b3a9dc1
Sharki642 <bruno.landrichter@gmail.com>
2026-06-15 09:58:44 +0200

    made a button and added a picture

--- ui_tesr.py
+++ ui_tesr.py
@@ -1,10 +1,23 @@
-
+#importing the tkinter module
 import tkinter as tk
 
 print("hello world!")
-
+#starting the window
 root = tk.Tk()
-root.title("Ui_testing")
+#title
+root.title("FileManager")
+root.minsize(400, 400)
+#inside
 
-root.configure(bg="grey")
+root.configure(bg="White")
+tk.Label(root, text="Welcome to FileManager").pack()
+image = tk.PhotoImage(file="image2.png")
+tk.Label(root, image=image).pack()
+
+
+Protokoll_de = tk.Button(root, text="Create German Protokoll")
+Protokoll_de.pack()
+
+
+
 root.mainloop()
